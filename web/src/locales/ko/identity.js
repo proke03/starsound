2caa1b92e390140947dffa72fc6c7b7fe16fdf88
Min Jang <proke03@naver.com>
2022-11-28 00:54:52 +0900

    resource: translate identity.js in Korean

--- web/src/locales/ko/identity.js
+++ web/src/locales/ko/identity.js
@@ -2,9 +2,9 @@ export const identity = {
   name: '별별소리',
   dns: 'starsound.in',
   landingPage: {
-    subtitle: 'All-in-one chat and forums for communities.',
-    description: 'The age of fragmented communities is over. Say goodbye to Reddit and Discord, and run your entire community on Starsound.',
-    download: 'Download for {{os}}',
-    openInBrowser: 'Open in Browser',
+    subtitle: '별별소리는 누구나 쉽게 만들고 공유할 수 있는 커뮤니티입니다.',
+    description: '누구나 쉽게 커뮤니티를 만들고 공유할 수 있습니다. 플래닛을 만들어서 친구들과 함께 소통하고, 새로운 사람들과 만나보세요.',
+    download: '{{os}}용 다운로드',
+    openInBrowser: '브라우저에서 열기',
   }
 }
