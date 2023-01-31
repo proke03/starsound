f054bdf662761ef26c8003d3cdb4e5f3a56a338a
JM <proke03@naver.com>
2023-01-31 15:57:39 +0900

    Revert "Lazyload Post component"
    This reverts commit da2276a8876776d65f4c1a5741b18df85ef4dfef.

--- web/src/components/post/Posts.jsx
+++ web/src/components/post/Posts.jsx
@@ -1,7 +1,7 @@
-import { useEffect, Suspense, lazy } from 'react'
+import { useEffect } from 'react'
 import { Virtuoso } from 'react-virtuoso'
 import { usePosts } from '@/components/post/usePosts'
-// import Post from '@/components/post/Post'
+import Post from '@/components/post/Post'
 import { IconSpinner } from '@/components/ui/icons/IconSpinner'
 import { useCallback, useRef } from 'react'
 import EndReached from '@/components/ui/EndReached'
@@ -9,10 +9,8 @@ import { useTranslation } from 'react-i18next'
 import { usePinnedPostsQuery } from '@/graphql/hooks'
 import { useStore } from '@/hooks/useStore'
 
-
 export default function Posts({ folderId, serverId, showServerName, header }) {
-  const { t } = useTranslation()
-  const Post = lazy(() => import('@/components/post/Post'))
+  const { t } = useTranslation();
   
   const virtuoso = useRef(null)
 
@@ -20,7 +18,7 @@ export default function Posts({ folderId, serverId, showServerName, header }) {
 
   const { postFetchDone, setPostFetchDone } = useStore(state => state)
   useEffect(() => {
-    if (!fetching && !postFetchDone) setPostFetchDone(true)
+    if (!fetching && !postFetchDone) setPostFetchDone(true);
   }, [fetching])
 
   const postRenderer = useCallback(
@@ -29,9 +27,7 @@ export default function Posts({ folderId, serverId, showServerName, header }) {
       if (!post) return <div style={{ height: '1px' }} /> // returning null or zero height breaks the virtuoso
       return (
         <div className="md:px-4 pb-1.5 px-0">
-          <Suspense fallback={<></>}>
-            <Post post={post} showServerName={showServerName} index={index} />
-          </Suspense>
+          <Post post={post} showServerName={showServerName} index={index} />
         </div>
       )
     },
